2516ecb2d1f046232dc925e0360702f7ba6ed738
Jorge Valdez <valdez.a.jorge@gmail.com>
2015-04-12 12:30:47 -0500

    Commenting because ur mom

--- components/DownloadRow.jsx
+++ components/DownloadRow.jsx
@@ -16,12 +16,32 @@ var DownloadRow = React.createClass({
     render: function() {
         return (
             <div>
-                <TextField id='video-url-text-field'/> <FlatButton label="Download this!" primary={true} onTouchTap={}/>
+                <TextField id='video-url-text-field'/>
+                <FlatButton label="Download this!" primary={true} onTouchTap={}/>
             </div>
         );
     },
 
     _handleSubmit: function(url) {
+        // This should probably be farther up the inheritance chain.
+        // Maybe in Application.jsx?
+    },
 
+    _chooseLocation: function() {
+        // This is where we should return the url to save to and append it to
+        // the file name of the download library. Maybe use fs or os .join
+        // and get that shit going.
+
+        // This should probably open up a nice little folder open window.
+        // See if Material UI has this included.
+    },
+
+    dehydrate: function () {
+        // So I don't know if anything should actually happen here..
+        // This would be where I take the internal state and return it to the
+        // front-end for rendering, but as of now, there IS no state.
+        // So fuck ur mum.
     }
+
+
 });
